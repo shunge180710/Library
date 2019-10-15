707e69b217ab4e44aee9b5bfab50b66a94bacb70
zhishun <zhi shun@imagedt.com>
2019-10-15 13:27:07 +0800

    Library init

---
+++ basic/config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=yii2basic',
+    'username' => 'root',
+    'password' => 'shunge20190228',
+    'charset' => 'utf8',
+    // shunge20190228
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ basic/views/site/entry-confirm.php
@@ -0,0 +1,8 @@
+<?php
+    use yii\helpers\Html;
+?>
+<p>hahahhahah</p>
+<ul>
+    <li><label for="">name</label>:<?=Html::encode($model->name)?></li>
+    <li><label for="">email</label>:<?=Html::encode($model->email)?></li>
+</ul>
